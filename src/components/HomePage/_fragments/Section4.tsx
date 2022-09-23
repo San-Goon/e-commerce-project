6e447508c8d8bd5c40425f2b58db5034a8b465ee
San-Goon <tigerlove95@naver.com>
2022-09-23 18:53:37 +0900

    feat: 메인페이지 Section4 구현

---
+++ src/components/HomePage/_fragments/Section4.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { Box, Flex, Image, Text } from '@chakra-ui/react';
+
+import ArrowRight from '@icons/System/ArrowRight';
+
+const Section4 = () => {
+  return (
+    <Box position="relative">
+      <Box position="absolute" left="5%" top="20%">
+        <Text textStyle="xl">
+          <Text as="span" fontWeight="700" color="primary.500">
+            인코스런
+          </Text>
+          가입하고
+          <Text fontWeight="700">전상품 1000원 혜택</Text>
+          <Text>받아보세요</Text>
+        </Text>
+        <Flex mt="20px" alignItems="center">
+          <Text textStyle="md">이벤트 상세보기</Text>
+          <ArrowRight />
+        </Flex>
+      </Box>
+      <Image src="/images/home/Layout.png" w="100%" h="auto" />
+    </Box>
+  );
+};
+
+export default Section4;
