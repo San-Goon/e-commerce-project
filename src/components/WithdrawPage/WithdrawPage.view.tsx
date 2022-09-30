4a8b68d1bbdf0c1b68e6f44db80c93be67a41acb
San-Goon <tigerlove95@naver.com>
2022-09-30 12:29:26 +0900

    chore: 탈퇴페이지 세부 디자인 수정

--- src/components/WithdrawPage/WithdrawPage.view.tsx
+++ src/components/WithdrawPage/WithdrawPage.view.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Controller, useFormContext } from 'react-hook-form';
 
 import {
-  Box, Button,
+  Box,
+  Button,
   Center,
   Flex,
   HStack,
@@ -21,21 +22,29 @@ const WithdrawPageView = ({ onSubmit }: any) => {
 
   return (
     <Box mt="130px">
-      <Text textStyle='lg' fontWeight='700'>회원탈퇴</Text>
-      <Text mt='80px' p='18px' bg='gray.100' textStyle='md'>
+      <Text textStyle="lg" fontWeight="700" ml="16px">
+        회원탈퇴
+      </Text>
+      <Text mt="80px" p="18px" bg="gray.100" textStyle="md">
         회원 탈퇴 시 개인 정보 및 인코스런에서 만들어진 모든 데이터는
         삭제됩니다. 한 번 삭제된 정보는 복구가 불가능합니다.
       </Text>
-      <Text  my='14px' fontStyle='md' fontWeight='700'>회원 정보</Text>
-      <Flex mb='20px'>
+      <Text my="14px" fontStyle="md" fontWeight="700" ml="16px">
+        회원 정보
+      </Text>
+      <Flex mb="20px" ml="16px">
         <Box>
-          <Text mt='15px' mb='10px'>이름</Text>
-          <Text mb='10px'>핸드폰 번호</Text>
+          <Text mt="15px" mb="10px">
+            이름
+          </Text>
+          <Text mb="10px">핸드폰 번호</Text>
           <Text>이메일 주소</Text>
         </Box>
-        <Box ml='24px' textColor='gray.700'>
-          <Text mt='15px' mb='10px'>조강훈</Text>
-          <Text mb='10px'>010-6611-2574</Text>
+        <Box ml="24px" textColor="gray.700">
+          <Text mt="15px" mb="10px">
+            조강훈
+          </Text>
+          <Text mb="10px">010-6611-2574</Text>
           <Text>tigerlove95@naver.com</Text>
         </Box>
       </Flex>
@@ -46,8 +55,8 @@ const WithdrawPageView = ({ onSubmit }: any) => {
         render={({ field: { value, onChange } }) => {
           console.log(value);
           return (
-            <FormHelper my="14px" label="탈퇴 사유" textStyle="md">
-              <RadioGroup onChange={onChange}>
+            <FormHelper my="14px" label="탈퇴 사유" textStyle="md" ml="16px">
+              <RadioGroup onChange={onChange} colorScheme="primary">
                 <Stack>
                   <Radio value="아이디 변경(재가입)">아이디 변경(재가입)</Radio>
                   <Radio value="낮은 구매 빈도">낮은 구매 빈도</Radio>
@@ -62,13 +71,20 @@ const WithdrawPageView = ({ onSubmit }: any) => {
           );
         }}
       />
-      <Box w="100vw" h="10px" mb='14px' backgroundColor="gray.100" />
-      <FormHelper mb="20px" label="인코스런을 입력해주세요." textStyle="md">
-        <Input fieldName="typo" />
+      <Box w="100vw" h="10px" mb="14px" backgroundColor="gray.100" />
+      <FormHelper
+        mb="20px"
+        label="인코스런을 입력해주세요."
+        textStyle="md"
+        mx="16px"
+      >
+        <Input fieldName="typo" w="90%" />
       </FormHelper>
-      <Flex mt='80px' mb='30px'>
-        <Button colorScheme='primary' variant='outline'>취소</Button>
-        <Button colorScheme='primary'>탈퇴하기</Button>
+      <Flex mt="80px" mb="30px" mx="16px">
+        <Button colorScheme="primary" variant="outline">
+          취소
+        </Button>
+        <Button colorScheme="primary">탈퇴하기</Button>
       </Flex>
     </Box>
   );
