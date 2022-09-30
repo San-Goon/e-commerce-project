b588af67549b6597aa03a998de4f09e7de784bb1
San-Goon <tigerlove95@naver.com>
2022-09-30 12:22:34 +0900

    feat: listpage api 기반으로 변경

--- src/components/ListPage/ListPage.tsx
+++ src/components/ListPage/ListPage.tsx
@@ -1,87 +1,97 @@
 import { Box, Button, Center, Flex, Image } from '@chakra-ui/react';
 
+import { useGetProductListQuery } from '@apis/product/ProductApi.query';
+
 import { StarIcon } from '../../generated/icons/MyIcons';
 
-const PRODUCT_LIST = [
-  {
-    id: 0,
-    name: '바스 & 샴푸',
-    description: '/images/productImg.png',
-    price: 27000,
-    capacity: 300,
-    tags: '#올인원 #클렌저 #마일드 #바스앤샴푸',
-    avgRate: 4.3,
-    reviewCount: 125,
-    created: '2022-09-25T09:11:43.482Z',
-  },
-];
-
 const ListPage = () => {
-  return (
-    <Center mt="120px">
-      {PRODUCT_LIST.map((item) => (
-        <Box
-          key={item.id}
-          m="0 16px 30px 16px"
-          maxW="sm"
-          borderWidth="1px"
-          overflow="hidden"
-          boxShadow="0px 0px 10px rgba(26, 26, 26, 0.1)"
-          borderRadius="20px"
-        >
-          <Image src={item.description} alt={item.name} />
+  const { data } = useGetProductListQuery();
 
-          <Box p="30px">
-            <Box display="flex" alignItems="baseline">
-              <Box fontWeight="700" letterSpacing="wide" fontSize="md">
-                {item.name}
-                <Box as="span" fontWeight="400" color="gray.700" ml="5px">
-                  {item.capacity}ml
+  if (data) {
+    return (
+      <Center mt="120px">
+        {data.results.map((item: any) => (
+          <Box
+            key={item.id}
+            m="0 16px 30px 16px"
+            maxW="sm"
+            borderWidth="1px"
+            overflow="hidden"
+            boxShadow="0px 0px 10px rgba(26, 26, 26, 0.1)"
+            borderRadius="20px"
+          >
+            <Image src={item.name} alt={item.name} />
+
+            <Box p="30px">
+              <Box display="flex" alignItems="baseline">
+                <Box fontWeight="700" letterSpacing="wide" fontSize="md">
+                  {item.name}
+                  <Box as="span" fontWeight="400" color="gray.700" ml="5px">
+                    {item.capacity}ml
+                  </Box>
                 </Box>
               </Box>
-            </Box>
 
-            <Box mt="10px" fontWeight="700" textStyle="lg" color="primary.500">
-              {item.price}
-              <Box as="span" color="black">
-                원
+              <Box
+                mt="10px"
+                fontWeight="700"
+                textStyle="lg"
+                color="primary.500"
+              >
+                {item.price}
+                <Box as="span" color="black">
+                  원
+                </Box>
               </Box>
-            </Box>
 
-            <Flex mt="2" alignItems="center" color="gray.700">
-              <StarIcon boxSize="16px" color="primary.500" />
-              <Box color="black" fontWeight="700" mr="3px">
-                {item.avgRate}
-              </Box>
-              (리뷰 {item.reviewCount}개)
-            </Flex>
-            <Box mt="25px" color="gray.700" textStyle="md">
-              {item.tags}
+              <Flex mt="2" alignItems="center" color="gray.700">
+                <StarIcon boxSize="16px" color="primary.500" />
+                <Box color="black" fontWeight="700" mr="3px">
+                  {item.avgRate}
+                </Box>
+                (리뷰 {item.reviewCount}개)
+              </Flex>
+              <Flex>
+                {item.tags.map((item: any) => {
+                  return (
+                    <Box
+                      key={item.id}
+                      mt="25px"
+                      color="gray.700"
+                      textStyle="md"
+                    >
+                      #{item.name}&ensp;
+                    </Box>
+                  );
+                })}
+              </Flex>
+              <Flex mt="20px" justifyContent="space-around" alignItems="center">
+                <Button
+                  w="150px"
+                  h="50px"
+                  borderRadius="25px"
+                  colorScheme="primary"
+                >
+                  바로구매
+                </Button>
+                <Button
+                  w="150px"
+                  h="50px"
+                  borderRadius="25px"
+                  colorScheme="primary"
+                  variant="outline"
+                >
+                  장바구니
+                </Button>
+              </Flex>
             </Box>
-            <Flex mt="20px" justifyContent="space-around" alignItems="center">
-              <Button
-                w="150px"
-                h="50px"
-                borderRadius="25px"
-                colorScheme="primary"
-              >
-                바로구매
-              </Button>
-              <Button
-                w="150px"
-                h="50px"
-                borderRadius="25px"
-                colorScheme="primary"
-                variant="outline"
-              >
-                장바구니
-              </Button>
-            </Flex>
           </Box>
-        </Box>
-      ))}
-    </Center>
-  );
+        ))}
+      </Center>
+    );
+  } else {
+    return <Box>로딩중</Box>;
+  }
 };
 
 export default ListPage;
