6ea4a9bd916d4680cdc8ca46c83878d0d0ce1b37
San-Goon <tigerlove95@naver.com>
2022-10-04 14:18:49 +0900

    chore: 타입 파일 분리

---
+++ src/utils/types.ts
@@ -0,0 +1,38 @@
+export interface IProductMap {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  capacity: number;
+  thumbnail: string;
+  tags: Array<{
+    id: number;
+    name: string;
+  }>;
+  avgRate: number;
+  reviewCount: number;
+}
+
+export interface IProductsList {
+  cursor: string;
+  results: Array<IProductMap>;
+}
+
+export interface IProduct {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  capacity: number;
+  detail: string;
+  photo: string;
+  reviewList: Array<{
+    id: number;
+    userId: number;
+    rate: number;
+    content: string;
+    created: string;
+  }>;
+  avgRate: number;
+  reviewCount: number;
+}
