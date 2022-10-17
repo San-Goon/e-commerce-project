158a82ba5309e8f1e0343034d46cb778a1372b20
San-Goon <tigerlove95@naver.com>
2022-10-17 18:11:18 +0900

    refactor: 상품 결제 API 연동 완료 / 배송 정보 localStorage 임시저장

--- src/components/PaymentPage/_Fragments/PaymentButton.tsx
+++ src/components/PaymentPage/_Fragments/PaymentButton.tsx
@@ -6,6 +6,10 @@ import { CONFIG } from '@config';
 import { Button, Flex } from '@chakra-ui/react';
 
 import { CartItemType } from '@apis/cart/CartApi.type';
+import {
+  usePostOrderMutation,
+  usePostOrderStatusMutation,
+} from '@apis/order/OrderApi.mutation';
 import { GetProductByIdReturnType } from '@apis/product/ProductApi.type';
 
 import { loadTossPayments } from '@tosspayments/payment-sdk';
@@ -14,37 +18,76 @@ import { setShipInfo } from '@utils/localStorage/shipInfo';
 import { FormDataType } from '../_hooks/usePaymentForm';
 
 interface PropsType {
-  orderInfo: {
-    userId: number;
-    amount: number;
-    orderId: string;
-    orderName: string;
-    customerName: string;
-  };
+  userId?: number;
+  price: number;
   productsList: (GetProductByIdReturnType & CartItemType)[];
 }
 
-const PaymentButton = ({ orderInfo, productsList }: PropsType) => {
-  const { handleSubmit } = useFormContext<FormDataType>();
+interface OrderInfoType {
+  amount: number;
+  orderId: string;
+  orderName: string;
+  customerName: string;
+  successUrl: string;
+  failUrl: string;
+}
 
-  const onClickPayment = () => {
+const PaymentButton = ({ userId, price, productsList }: PropsType) => {
+  const { handleSubmit } = useFormContext<FormDataType>();
+  const { mutateAsync: postOrderMutate } = usePostOrderMutation();
+  const { mutateAsync: postOrderStatusMutate } = usePostOrderStatusMutation();
+
+  const onClickPayment = (orderInfo: OrderInfoType) => {
     loadTossPayments(CONFIG.PAYMENT_CLIENT_KEY as string).then(
       (tossPayments) => {
-        tossPayments.requestPayment('카드', {
-          // amount: orderInfo.amount,
-          amount: 1,
-          orderId: orderInfo.orderId,
-          orderName: orderInfo.orderName,
-          customerName: orderInfo.customerName,
-          successUrl: 'http://localhost:3000/success',
-          failUrl: 'http://localhost:3000/cart',
-        });
+        tossPayments.requestPayment('카드', orderInfo);
       },
     );
   };
-  const onSubmit: SubmitHandler<FormDataType> = (data) => {
-    setShipInfo({ ...data, orderInfo, productsList });
-    onClickPayment();
+  const onSubmit: SubmitHandler<FormDataType> = async (data) => {
+    try {
+      const {
+        PIAgree,
+        userAddress,
+        userExtraAddress,
+        shipAddress,
+        shipExtraAddress,
+        ...rest
+      } = data;
+      const mutateData = {
+        userId,
+        price,
+        userAddrDetail: userAddress + userExtraAddress,
+        shipAddrDetail: shipAddress + shipExtraAddress,
+        ...rest,
+      };
+      const { id: orderId } = await postOrderMutate(mutateData);
+      for (const { productId, count } of productsList) {
+        await postOrderStatusMutate({
+          orderId,
+          productId,
+          count,
+        });
+      }
+      const orderInfo = {
+        // amount: price,
+        amount: 1,
+        orderId,
+        orderName:
+          productsList.length === 1
+            ? `${productsList[0].name}`
+            : `${productsList[0].name} 외 ${productsList.length}건`,
+        customerName: data.userName,
+        successUrl: 'http://localhost:3000/success',
+        failUrl: 'http://localhost:3000/cart',
+      };
+      const count = productsList.length;
+      const cartIds = productsList.map((item) => item.id);
+      await setShipInfo({ count, cartIds });
+      await onClickPayment(orderInfo);
+    } catch (e) {
+      console.error('ERROR!', e);
+    }
   };
 
   return (
