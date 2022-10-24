25d2c28ff403e9974804cd97092254007bbba21c
San-Goon <tigerlove95@naver.com>
2022-10-24 19:04:42 +0900

    refactor : 마이페이지 refactoring

--- src/components/MyPage/MyPage.tsx
+++ src/components/MyPage/MyPage.tsx
@@ -1,39 +1,24 @@
-import { useRouter } from 'next/router';
-import React from 'react';
+import Link from 'next/link';
+import React, { useCallback } from 'react';
 
-import { Box, Center, Flex, Text } from '@chakra-ui/react';
+import { Box, Center, Flex, Text, useDisclosure } from '@chakra-ui/react';
 
 import { useGetMeQuery } from '@apis/user/UserApi.query';
 
+import LogoutModal from '@components/common/LogoutModal';
 import ArrowRight from '@icons/System/ArrowRight';
-
-import { deleteToken } from '@utils/cookie/token';
 
 import { BookIcon, BoxIcon, InfoIcon } from '../../generated/icons/MyIcons';
 
 const MyPageComponent = () => {
   const { data } = useGetMeQuery();
-  console.log('data', data);
-  const router = useRouter();
-  const onClickLogout = () => {
-    deleteToken();
-    router.push('/login');
-  };
-  const onClickWithdraw = () => {
-    router.push({ pathname: '/withdraw', query: data });
-  };
-  const onClickModify = () => {
-    router.push({ pathname: '/modify', query: data });
-  };
-  const onClickMyReview = () => {
-    router.push({ pathname: '/myreview', query: data?.id.toString() });
-  };
-  const onClickHistory = () => {
-    router.push({ pathname: '/history', query: data?.id.toString() });
-  };
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const onClickLogout = useCallback(() => {
+    onOpen();
+  }, [onOpen]);
 
   return (
-    <Box mt="150px">
+    <Box mt="50px">
       <Text mx="16px" textStyle="lg" fontWeight="700">
         {data?.nickname}
       </Text>
@@ -47,39 +32,30 @@ const MyPageComponent = () => {
         justifyContent="space-between"
         alignItems="center"
       >
-        <Center flexDirection="column" cursor="pointer" onClick={onClickModify}>
-          <InfoIcon boxSize="50px" color="primary.500" />
-          <Text>회원정보 수정</Text>
-        </Center>
-        <Center
-          flexDirection="column"
-          cursor="pointer"
-          onClick={onClickHistory}
-        >
-          <BoxIcon boxSize="50px" color="primary.500" />
-          <Text>주문 내역</Text>
-        </Center>
-        <Center
-          flexDirection="column"
-          cursor="pointer"
-          onClick={onClickMyReview}
-        >
-          <BookIcon boxSize="50px" color="primary.500" />
-          <Text>내 상품 리뷰</Text>
-        </Center>
+        {MENU_ITEMS.map(({ id, href, title, icon }) => {
+          return (
+            <Link key={id} href={href} passHref>
+              <Center flexDirection="column" cursor="pointer">
+                {icon}
+                <Text>{title}</Text>
+              </Center>
+            </Link>
+          );
+        })}
       </Center>
       <Flex h="10px" backgroundColor="gray.100" />
-      <Flex
-        mx="16px"
-        h="60px"
-        justifyContent="space-between"
-        alignItems="center"
-        cursor="pointer"
-        onClick={onClickWithdraw}
-      >
-        <Text>회원탈퇴</Text>
-        <ArrowRight boxSize="24px" />
-      </Flex>
+      <Link href="/withdraw" passHref>
+        <Flex
+          mx="16px"
+          h="60px"
+          justifyContent="space-between"
+          alignItems="center"
+          cursor="pointer"
+        >
+          <Text>회원탈퇴</Text>
+          <ArrowRight boxSize="24px" />
+        </Flex>
+      </Link>
       <Flex
         mx="16px"
         h="60px"
@@ -92,8 +68,30 @@ const MyPageComponent = () => {
         <ArrowRight boxSize="24px" />
       </Flex>
       <Flex h="30px" backgroundColor="gray.100" />
+      <LogoutModal isOpen={isOpen} onClose={onClose} />
     </Box>
   );
 };
 
 export default MyPageComponent;
+
+const MENU_ITEMS = [
+  {
+    id: 1,
+    href: '/modify',
+    title: '회원정보 수정',
+    icon: <InfoIcon boxSize="50px" color="primary.500" />,
+  },
+  {
+    id: 2,
+    href: '/history',
+    title: '주문내역',
+    icon: <BoxIcon boxSize="50px" color="primary.500" />,
+  },
+  {
+    id: 3,
+    href: '/myreview',
+    title: '내 상품 리뷰',
+    icon: <BookIcon boxSize="50px" color="primary.500" />,
+  },
+];
