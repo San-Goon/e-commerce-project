0329d50d22a1de7407508c0f9e4eb05a05453c54
San-Goon <tigerlove95@naver.com>
2022-10-27 15:47:51 +0900

    fix: vercel 에러로 인한 document 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,43 +16,17 @@ const SITE_TITLE = 'TOKTOKHAN.DEV';
 const SITE_DESCRIPTION = '디지털프로덕트의 TOKTOK한 경험';
 const SITE_IMAGE = '/images/new_og.png';
 
-const GOOGLE_ANALYTICS_ID = 'G-입력해주세요';
-
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
 
-  redirectIEtoEdge() {
-    return {
-      __html: `
-      if(/MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
-        window.location = 'microsoft-edge:' + window.location;
-        setTimeout(function() {
-          window.location = 'https://go.microsoft.com/fwlink/?linkid=2135547';
-        }, 1);
-      }`,
-    };
-  }
-
-  setGoogleAnalytics() {
-    return {
-      __html: `        
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${GOOGLE_ANALYTICS_ID}');
-      `,
-    };
-  }
   render() {
     return (
       <Html>
         <Head>
-          <script dangerouslySetInnerHTML={this.redirectIEtoEdge()} />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
           {/* SEO */}
           <link rel="apple-touch-icon" href="/icons/120.png" />
           <link rel="apple-touch-icon" sizes="152x152" href="/icons/152.png" />
@@ -80,11 +54,6 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
 
           {/* Global site tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
-          ></script>
-          <script dangerouslySetInnerHTML={this.setGoogleAnalytics()} />
         </Head>
         <body>
           <ColorModeScript initialColorMode={config.initialColorMode} />
