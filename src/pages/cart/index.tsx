3e04647cd13083260ea4b3fb136d0a777c641757
San-Goon <tigerlove95@naver.com>
2022-11-08 16:36:33 +0900

    feat: cart page SSR 구현

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,5 +1,37 @@
+import { GetServerSideProps } from 'next';
+
+import cookie from 'cookie';
+
+import cartApi from '@apis/cart/CartApi';
+import { GetCartReturnType } from '@apis/cart/CartApi.type';
+import productApi from '@apis/product/ProductApi';
+
 import CartPage from '@components/CartPage';
 import HomeLayout from '@components/common/@Layout/HomeLayout';
+
+import { QueryClient, dehydrate } from '@tanstack/react-query';
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const queryClient = new QueryClient();
+  const myCookie = cookie.parse((ctx.req && ctx.req.headers.cookie) || '');
+  const userId: string = myCookie['@id'];
+  await queryClient.prefetchQuery(['get-cart'], () => cartApi.getCart(userId));
+  const data = (
+    queryClient.getQueryData(['get-cart']) as GetCartReturnType[]
+  )[0].cartitem;
+  await Promise.all(
+    data.map(async ({ productId }) => {
+      await queryClient.prefetchQuery(['product-by-id', productId], () =>
+        productApi.getProductById(productId),
+      );
+    }),
+  );
+  return {
+    props: {
+      dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
+    },
+  };
+};
 
 const Cart = () => {
   return <HomeLayout content={<CartPage />} />;
