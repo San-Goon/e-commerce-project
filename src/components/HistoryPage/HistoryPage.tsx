188a5de45a36ab9dbb59d83364deb6e3cfd898bb
San-Goon <tigerlove95@naver.com>
2022-11-14 15:53:06 +0900

    feat: 주문 내역 페이지 로딩 구현

--- src/components/HistoryPage/HistoryPage.tsx
+++ src/components/HistoryPage/HistoryPage.tsx
@@ -2,10 +2,11 @@ import React, { useMemo, useState } from 'react';
 import { FormProvider } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Center, CircularProgress, Text } from '@chakra-ui/react';
 
 import { useGetOrderStatusQuery } from '@apis/order/OrderApi.query';
 import { PostOrderStatusReturnType } from '@apis/order/OrderApi.type';
+import productApi from '@apis/product/ProductApi';
 import { useGetMeQuery } from '@apis/user/UserApi.query';
 import { queryKeySliceActions } from '@features/queryKey/queryKeySlice';
 
@@ -14,6 +15,8 @@ import useModifyShippingInfoForm, {
   defaultValues,
 } from '@components/HistoryPage/_hooks/useModifyShippingInfoForm';
 import Pagination from '@components/common/Pagination';
+
+import { useQueries } from '@tanstack/react-query';
 
 const HistoryPage = () => {
   const formData = useModifyShippingInfoForm({ defaultValues });
@@ -58,6 +61,29 @@ const HistoryPage = () => {
     return undefined;
   }, [products]);
 
+  const queries = useMemo(() => {
+    if (products) {
+      return products?.results?.map((result) => ({
+        queryKey: ['product-by-id', result.productId],
+        queryFn: () => productApi.getProductById(result.productId),
+        enabled: !!products,
+        staleTime: Infinity,
+      }));
+    }
+    return [];
+  }, [products]);
+
+  const productData = useQueries({
+    queries,
+  });
+
+  const isRender = useMemo(() => {
+    if (productData.length !== 0) {
+      if (productData.every((data) => data.status === 'success')) return true;
+    }
+    return false;
+  }, [productData]);
+
   return (
     <FormProvider {...formData}>
       {isSuccess ? (
@@ -66,19 +92,26 @@ const HistoryPage = () => {
             <Text textStyle="lg" fontWeight="700">
               주문내역
             </Text>
-            <Box mt="80px">
-              {productList &&
-                Object.keys(productList).map((orderId) => {
-                  return (
-                    <React.Fragment key={orderId}>
-                      <OrderListSection
-                        productList={productList}
-                        orderId={orderId}
-                      />
-                    </React.Fragment>
-                  );
-                })}
-            </Box>
+            {isRender ? (
+              <Box mt="80px">
+                {productList &&
+                  Object.keys(productList).map((orderId) => {
+                    return (
+                      <React.Fragment key={orderId}>
+                        <OrderListSection
+                          productList={productList}
+                          orderId={orderId}
+                        />
+                      </React.Fragment>
+                    );
+                  })}
+              </Box>
+            ) : (
+              <Center flexDir="column" h="107px">
+                <CircularProgress isIndeterminate color="primary.500" />
+                <Text mt="5px">주문내역을 불러오고 있습니다...</Text>
+              </Center>
+            )}
             {numPages && (
               <Pagination numPages={numPages} page={page} setPage={setPage} />
             )}
